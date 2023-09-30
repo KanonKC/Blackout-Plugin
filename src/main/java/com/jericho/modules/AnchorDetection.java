7a0631ae86a014bd6fb81fd1e5551634f30f2707
KanonKC <kanonkce@gmail.com>
2023-10-01 00:12:34 +0700

    Archor detection and panelty

---
+++ src/main/java/com/jericho/modules/AnchorDetection.java
@@ -0,0 +1,103 @@
+package com.jericho.modules;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class AnchorDetection {
+
+    String HUNTER_TAG = "hunter";
+    String SURVIVOR_TAG = "survivor";
+
+    Integer count = 0;
+    Integer threshold = 10;
+    HashMap<Player,Location[]> playerPreviousLocations = new HashMap<Player,Location[]>();
+    HashMap<Player,Boolean[]> hunterNearbyPlayers = new HashMap<Player,Boolean[]>();
+
+    List<Player> survivors;
+    Player hunter;
+
+    public AnchorDetection(List<Player> survivors, Player hunter) {
+        this.survivors = survivors;
+        this.hunter = hunter;
+    }
+
+    public void impluse() {
+        if (count >= threshold) {
+                    survivors.forEach(player -> {
+                        Double averageMovingDistance = getAverageMovingDistance(playerPreviousLocations.get(player));
+                        if (averageMovingDistance < 1 && noHunterNearby(hunterNearbyPlayers.get(player))) {
+                            
+                            PotionEffect glowEffect = player.getPotionEffect(PotionEffectType.GLOWING);
+                            if (glowEffect == null) {
+                                player.sendMessage("You are now being " + ChatColor.RED +  "EXPOSED" + ChatColor.WHITE + " after standing still for too long!");
+                                hunter.sendMessage(ChatColor.AQUA + player.getName() + ChatColor.WHITE + "'s location has been revealed!");
+                            }
+                            
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 4*20, 0, false, false, false));
+                            
+                        }
+                    });
+                }
+
+                survivors.forEach(player -> {
+
+                    Location[] previousLocations = playerPreviousLocations.get(player);
+                    if (previousLocations == null) {
+                        previousLocations = new Location[threshold];
+                    }
+                    previousLocations[count % threshold] = player.getLocation();
+                    playerPreviousLocations.put(player, previousLocations);
+
+                    Boolean[] previousHunterNearbyPlayers = hunterNearbyPlayers.get(player);
+                    if (previousHunterNearbyPlayers == null) {
+                        previousHunterNearbyPlayers = new Boolean[threshold];
+                    }
+
+                    Boolean hunterNearby = false;
+                    for (Entity entity: player.getNearbyEntities(10,10,10)) {
+                        if (entity instanceof Player) {
+                            Player nearbyPlayer = (Player) entity;
+                            if (nearbyPlayer.getScoreboardTags().contains(HUNTER_TAG)) {
+                                previousHunterNearbyPlayers[count % threshold] = true;
+                                hunterNearby = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!hunterNearby) {
+                        previousHunterNearbyPlayers[count % threshold] = false;
+                    }
+
+                    hunterNearbyPlayers.put(player, previousHunterNearbyPlayers);
+                    
+                });
+                count++;
+    }
+
+    private Double getAverageMovingDistance(Location[] locations) {
+        Double totalDistance = 0.0;
+        for (int i = 0; i < locations.length - 1; i++) {
+            totalDistance += locations[i].distance(locations[i+1]);
+        }
+        return totalDistance / (locations.length - 1);
+    }
+
+    private Boolean noHunterNearby(Boolean[] booleans) {
+        for (Boolean bool: booleans) {
+            if (bool) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
--- src/main/java/com/jericho/modules/GameSystem.java
+++ src/main/java/com/jericho/modules/GameSystem.java
@@ -1,5 +1,6 @@
 package com.jericho.modules;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -11,6 +12,7 @@ import org.bukkit.World;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -36,23 +38,30 @@ public class GameSystem implements Listener {
 
     NamespacedKey taskIdKey;
     NamespacedKey timerKey;
+    NamespacedKey inGameKey;
 
     String HUNTER_TAG = "hunter";
     String SURVIVOR_TAG = "survivor";
 
-    Location lobbyLocation = new Location(world, -42,11,-89);
+    Location lobbyLocation = new Location(world, -72,-34,-88);
 
     Location hunterSpawnLocations[] = {
-        new Location(world,-64, 47, -98)
+        new Location(world,-61 ,42, -87)
     };
     Location survivorSpawnLocations[] = {
-        new Location(world,-95, 43, -89),
-        new Location(world,-82, 42, -43),
-        new Location(world,0, 39, -47),
-        new Location(world,-4, 42, -127),
+        new Location(world,-80, 58,-98),
+        new Location(world,-70, 67,-93),
+        new Location(world,-72, 50,-76),
+        new Location(world,-58, 50,-84),
+        new Location(world,-87, 59,-75),
+        new Location(world,-74, 47,-82),
+        new Location(world,-81, 41,-84),
+        new Location(world,-64, 62,-98),
+        new Location(world,-84, 62,-78),
+        new Location(world,-83, 46,-98)
     };
 
-    double gameDurationSeconds = 120;
+    double gameDurationSeconds = 300;
     
     BossBar timerBar;
     BukkitTask timerTask;
@@ -64,6 +73,7 @@ public class GameSystem implements Listener {
         this.world = world;
         taskIdKey = new NamespacedKey(plugin, "taskId");
         timerKey = new NamespacedKey(plugin, "timerKey");
+        inGameKey = new NamespacedKey(plugin, "inGameKey");
 
         timerBar = Bukkit.createBossBar(timerKey,"Time Left", BarColor.RED, BarStyle.SEGMENTED_20);
     }
@@ -74,6 +84,7 @@ public class GameSystem implements Listener {
             team = Bukkit.getScoreboardManager().getMainScoreboard().registerNewTeam("blackout");
             team.setAllowFriendlyFire(false);
             team.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.NEVER);
+            team.allowFriendlyFire();
         }
         return team;
     }
@@ -84,6 +95,7 @@ public class GameSystem implements Listener {
     public void start() {
         // Pick a random player to be the hunter
         List<Player> allPlayers = world.getPlayers();
+        allPlayers.removeIf(player -> player.getGameMode() == GameMode.SPECTATOR);
 
         if (allPlayers.size() < 2) {
             Bukkit.broadcastMessage(ChatColor.RED + "Not enough players to start the game");
@@ -96,6 +108,7 @@ public class GameSystem implements Listener {
         hunter.addScoreboardTag(HUNTER_TAG);
 
         List<Player> survivors = world.getPlayers();
+        survivors.removeIf(player -> player.getGameMode() == GameMode.SPECTATOR);
         
         survivors.remove(hunter);
         survivors.forEach(player -> {
@@ -114,19 +127,21 @@ public class GameSystem implements Listener {
             player.teleport(world.getBlockAt(survivorSpawnLocation).getLocation());
 
             player.sendTitle(ChatColor.AQUA + "You are a Survivor", "Stay alive for 5 minutes", 10, 70, 20);
+            player.sendMessage(ChatColor.RED + hunter.getName() + " is the Hunter");
+            player.setGameMode(GameMode.ADVENTURE);
         });
 
         // Applied effect to hunter
         hunter.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999*20, 0, false, false, false));
-        hunter.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 999999*20, 5, false, false, false));
+        hunter.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 999999*20, 4, false, false, false));
+        hunter.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999*20, 2, false, false, false));
+        hunter.setGameMode(GameMode.ADVENTURE);
 
         // Random teleport hunter to a hunter spawn location
         Location hunterSpawnLocation = this.hunterSpawnLocations[random.nextInt(this.hunterSpawnLocations.length)];
         hunter.teleport(world.getBlockAt(hunterSpawnLocation).getLocation());
 
         hunter.sendTitle(ChatColor.RED + "You are the Hunter", "Take down all survivors in 5 minutes", 10, 70, 20);
-
-
 
         // Display timer bar
         timerBar.setProgress(1);
@@ -138,8 +153,12 @@ public class GameSystem implements Listener {
 
         // Start timer
         timerTask = new BukkitRunnable() {
+
+            AnchorDetection anchorDetection = new AnchorDetection(survivors, hunter);
+
             @Override
             public void run() {
+                anchorDetection.impluse();
                 try {
                     timerBar.setProgress(timerBar.getProgress() - (1/gameDurationSeconds));
                 }
@@ -152,6 +171,7 @@ public class GameSystem implements Listener {
         // Save timer task id to world persistent data
         PersistentDataContainer dataContainer = world.getPersistentDataContainer();
         dataContainer.set(taskIdKey, PersistentDataType.INTEGER, timerTask.getTaskId());
+        dataContainer.set(inGameKey, PersistentDataType.BOOLEAN, true);
     }
 
     /**
@@ -182,6 +202,7 @@ public class GameSystem implements Listener {
 
             player.removePotionEffect(PotionEffectType.BLINDNESS);
             player.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+            player.removePotionEffect(PotionEffectType.SPEED);
             player.setGameMode(GameMode.ADVENTURE);
             player.teleport(world.getBlockAt(lobbyLocation).getLocation());
             blackoutTeam.removeEntry(player.getName());
@@ -201,9 +222,15 @@ public class GameSystem implements Listener {
             
         }
 
+        dataContainer.set(inGameKey, PersistentDataType.BOOLEAN, false);
+
     }
 
     private void endGameIfNoSurvivorLeft() {
+        PersistentDataContainer dataContainer = world.getPersistentDataContainer();
+        if (dataContainer.get(inGameKey, PersistentDataType.BOOLEAN) == null || dataContainer.get(inGameKey, PersistentDataType.BOOLEAN) == false) {
+            return;
+        }
         for (Player player: world.getPlayers()) {
             if (player.getScoreboardTags().contains(SURVIVOR_TAG)) {
                 return;
@@ -215,6 +242,10 @@ public class GameSystem implements Listener {
     @EventHandler
     public void survivorDeathEvent(PlayerDeathEvent e) {
         Player player = e.getEntity();
+        PersistentDataContainer dataContainer = world.getPersistentDataContainer();
+        if (dataContainer.get(inGameKey, PersistentDataType.BOOLEAN) == null || dataContainer.get(inGameKey, PersistentDataType.BOOLEAN) == false) {
+            return;
+        }
         // Edit death message
         e.setDeathMessage(ChatColor.RED + player.getName() + ChatColor.WHITE + " has been slained");
         if (player.getScoreboardTags().contains(SURVIVOR_TAG)) {
